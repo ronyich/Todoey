5192d9bed3fc18d9717acb291f9a4de459b9d6f2
Ron Yi <a3042a@hotmail.com>
2018-07-30 18:27:14 +0800

    Data persisted using UserDefaults method.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
-    
-    //var addTextField = UITextField()
+    //建立一個標準版的UserDefaults，用來永久儲存
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //讓app一開始就先檢索"ToDoListArray"目錄裡的資料，讓以前存過資料顯示出來
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     
@@ -57,22 +62,27 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             alert.addTextField { (alertTextField) in
+                //把輸入的內容存到itemArray
                 self.itemArray.append(textField.text!)
                 
+                //把資料存到"ToDoListArray"目錄裡，還需在viewDidLoad裡設定檢索(讀取)itemArray
+                self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
+                
+                //reload之後才會將更新的資料顯示在tableView中
                 self.tableView.reloadData()
             }
         }
         
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         //點選+後出現文字輸入框，alert的preferredStyle不能選.actionSheet，不支援文字輸入框功能
+        //輸入玩文字後按下Add Item，會執行該action裡面的內容
         alert.addTextField { (alertTextField) in
 
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
           
         }
-        
-        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         alert.addAction(action)
         alert.addAction(actionCancel)
